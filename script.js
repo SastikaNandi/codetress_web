cb7abae53bd37d052a87004e85b989c406470309
SastikaNandi <nandiswastika26@gmail.com>
2025-02-25 14:31:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.enqform').on('submit',function(e){
+        e.preventDefault();
+        
+        var phone=$('.phoneno').val();
+        var name=$('.nameval').val();
+
+      if(phone.length!==10){
+        
+        $('.erroemessagephone').show();
+      }
+    
+    })
+});
